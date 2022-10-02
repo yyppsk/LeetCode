7fecc9b179013e49ae29fa20dd02567e6daf5fbf
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-02 19:35:12 +0530

    2 More Solutions

--- From October22/26RemoveDuplicatesfromSortedArray.cpp
+++ From October22/26RemoveDuplicatesfromSortedArray.cpp
@@ -25,9 +25,46 @@ int removeDuplicatessolution1(vector<int> &nums)
     }
     return index;
 }
+int removeDuplicatessolution2(vector<int> &nums)
+{
+    int start = 0;
+    int end = 1;
+    int index = 0;
+    while (start <= end && end < nums.size())
+    {
+        if (nums[start] == nums[end])
+        {
+            end += 1;
+        }
+        else
+        {
+            nums[index] = nums[start];
+            start = end;
+            index += 1;
+        }
+    }
+    for (auto i : nums)
+    {
+        cout << i << " ";
+    }
+    return index + 1;
+}
+int removesol3(vector<int> &nums)
+{
+    int i = 0;
+    for (int j = 1; j < nums.size(); j++)
+    {
+        if (nums[i] != nums[j])
+        {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
 int main()
 {
     vector<int> nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
-    cout << removeDuplicatessolution1(nums);
+    cout << removesol3(nums);
     return 0;
 }
