2ae0a0597c04beea60a36d2a552b70770c0732de
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-02 18:55:46 +0530

    First Bad Version

--- From October22/278FirstBadVersion.cpp
+++ From October22/278FirstBadVersion.cpp
@@ -3,10 +3,32 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-void solve()
+bool isBadVersion(int version)
 {
+    int bad = 5;
+    if (version >= bad)
+    {
+        return true;
+    }
+    return false;
+}
+int firstBadVersion(int n)
+{
+    int start = 1, end = n;
+    while (start < end)
+    {
+        int mid = start + (end - start) / 2;
+        if (!isBadVersion(mid))
+            start = mid + 1;
+        else
+            end = mid;
+    }
+    return start;
 }
 int main()
 {
+    int n = 10;
+    int bad = 5;
+    cout << firstBadVersion(n);
     return 0;
 }
