de2d63babaac7fb0a6df2676346400542f798db8
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-05 02:46:25 +0530

    58 Done

--- From October22/58LengthofLastWord.cpp
+++ From October22/58LengthofLastWord.cpp
@@ -5,9 +5,39 @@
 using namespace std;
 int lengthOfLastWord(string s)
 {
+    if (s.size() == 0)
+        return 0;
+    if (s.size() == 1 && s[0] != ' ')
+    {
+        return 1;
+    }
+    int end = s.size() - 1;
+    int trailspaces = 0;
+    int trackerword = 0;
+    if (s[end] == ' ')
+    {
+        int trailspaces = 1;
+    }
+    int length = 0;
+    do
+    {
+        if (s[end] != ' ')
+        {
+            trackerword = 1;
+            ++length;
+        }
+        if ((trackerword == 1) && (s[end] == ' '))
+        {
+            return length;
+        }
+        // printf("%c Length %d\n", s[end], length);
+        end--;
+    } while (end >= 0);
+    return length;
 }
 int main()
 {
-    string str = "   fly me   to   the moon  ";
+    string str = "aa";
+    cout << lengthOfLastWord(str);
     return 0;
 }
