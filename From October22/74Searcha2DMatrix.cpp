fcafd0ddc3ca91bfff12099c53b5e312cb370263
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-06 00:14:06 +0530

    74 Done

--- From October22/74Searcha2DMatrix.cpp
+++ From October22/74Searcha2DMatrix.cpp
@@ -7,12 +7,26 @@ bool searchMatrix(vector<vector<int>> &matrix, int target)
 {
     if (!matrix.size())
         return false;
-    int n = matrix.size();
-    
+    int row = matrix.size();
+    int col = matrix[0].size();
+
+    int lo = 0;
+    int hi = (row * col) - 1;
+    while (lo <= hi)
+    {
+        int mid = lo + (hi - lo) / 2;
+        if (matrix[mid / col][mid % col] == target)
+            return true;
+        if (matrix[mid / col][mid % col] < target)
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+    }
+    return false;
 }
 int main()
 {
     vector<vector<int>> arr = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
-    searchMatrix(arr, 3);
+    cout << searchMatrix(arr, 5);
     return 0;
 }
