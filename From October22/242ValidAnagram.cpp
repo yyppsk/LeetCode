005c27ddf13f5ac487f22b6f5e56b11849aed157
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-06 19:03:35 +0530

    242

--- From October22/242ValidAnagram.cpp
+++ From October22/242ValidAnagram.cpp
@@ -1,12 +1,42 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
 using namespace std;
-void solve(){
-
+bool isAnagram(string s, string t)
+{
+    if (s.size() != t.size())
+        return false;
+    unordered_map<char, int> map;
+    for (int i = 0; i < s.size(); i++)
+    {
+        map[s[i]]++;
+    }
+    unordered_map<char, int> map2;
+    for (int i = 0; i < s.size(); i++)
+    {
+        map2[t[i]]++;
+    }
+    /*
+    for (auto i : map)
+    {
+        cout << i.first << ":" << i.second << endl;
+    }
+    for (auto i : map2)
+    {
+        cout << i.first << ":" << i.second << endl;
+    }
+    */
+    for (auto ch : s)
+    {
+        if (map[ch] != map2[ch])
+            return false;
+    }
+    return true;
 }
 int main()
 {
+    cout << isAnagram("WilliamShakespeare", "Iamaweakishspeller");
     return 0;
 }
