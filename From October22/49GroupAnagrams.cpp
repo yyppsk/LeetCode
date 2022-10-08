1b8a601dd671288b69178ae537962e2269c55528
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-08 23:22:39 +0530

    49

--- From October22/49GroupAnagrams.cpp
+++ From October22/49GroupAnagrams.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <unordered_map>
 #include <string>
 using namespace std;
-void solve()
+vector<vector<string>> groupAnagrams(vector<string> &strs)
 {
+    map<map<char, int>, vector<string>> mpp;
+    for (string s : strs)
+    {
+        map<char, int> mpp2;
+        for (char ch : s)
+            mpp2[ch]++;
+        mpp[mpp2].push_back(s);
+    }
+    for(Atu)
+    vector<vector<string>> ans;
+    for (auto x : mpp)
+    {
+        ans.push_back(x.second);
+    }
+    return ans;
 }
 int main()
 {
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
     return 0;
 }
