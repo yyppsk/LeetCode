400f9d5caf443c42792786adfa5e2825ffbe81f6
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-09 00:03:39 +0530

    1855

--- From October22/1855MaximumDistanceBetweenaPairofValues.cpp
+++ From October22/1855MaximumDistanceBetweenaPairofValues.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int maxDistance(vector<int> &nums1, vector<int> &nums2)
 {
     // i <= j and nums1[i] <= nums2[j]. The distance of the pair is j - i​​​​
-    vector<pair<int, int>> res;
     int maxi = INT_MIN;
     for (int i = 0; i < nums1.size(); i++)
     {
@@ -15,27 +14,20 @@ int maxDistance(vector<int> &nums1, vector<int> &nums2)
         while (start <= end)
         {
             int mid = start + (end - start) / 2;
-            if ((mid <= nums2[mid]) && (i <= mid))
+            if ((nums1[i] <= nums2[mid]))
             {
-                res.push_back({mid, i});
-            }
-            if ((mid < nums2[mid]))
-            {
+                maxi = max(mid - i, maxi);
                 start = mid + 1;
             }
-            if ((mid > nums2[mid]))
+            else if ((nums1[i] > nums2[mid]))
                 end = mid - 1;
         }
     }
-    for (auto i : res)
-    {
-        cout << i.first << ":" << i.second << endl;
-    }
-    return 0;
+    return maxi == INT_MIN ? 0 : maxi;
 }
 int main()
 {
     vector<int> nums1 = {55, 30, 5, 4, 2}, nums2 = {100, 20, 10, 10, 5};
-    maxDistance(nums1, nums2);
+    cout << maxDistance(nums1, nums2);
     return 0;
 }
