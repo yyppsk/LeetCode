284b245f2d7d5ba62a9b2e7c4d5d76f46f8aa646
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-09 14:09:37 +0530

    Findmin

--- From October22/153FindMinimuminRotatedSortedArray.cpp
+++ From October22/153FindMinimuminRotatedSortedArray.cpp
@@ -3,10 +3,16 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-void solve()
+int findpivot(vector<int> arr, int start, int end)
 {
+}
+int findMin(vector<int> &nums)
+{
+    int end = nums.size();
+    int pivot = findpivot(nums, 0, end);
 }
 int main()
 {
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
     return 0;
 }
