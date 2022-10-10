2a87add4cf9dbce739daa8836f71fa5a4ea5a5e3
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-10 11:34:54 +0530

    881 M1

--- From October22/881BoatstoSavePeople.cpp
+++ From October22/881BoatstoSavePeople.cpp
@@ -3,10 +3,32 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-void solve()
+int numRescueBoats(vector<int> &people, int limit)
 {
+    // trying to find possible pairs
+    int front = 0, end = people.size() - 1;
+    int count = 0;
+    sort(people.begin(), people.end());
+    while (front <= end)
+    {
+        if ((people[front] + people[end]) <= limit)
+        {
+            front++;
+            end--;
+            count++;
+        }
+        else
+        {
+            end--;
+            count++;
+        }
+    }
+    return count;
 }
-int main()
+5 int main()
 {
+    vector<int> people = {3, 2, 2, 1};
+    int limit = 3;
+    cout << numRescueBoats(people, limit);
     return 0;
 }
