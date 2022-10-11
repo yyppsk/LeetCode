108cafecec9d745c1cc02cf6aa05c1337ca0fa66
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-11 18:10:41 +0530

    Function 1004

---
+++ From October22/1004MaxConsecutiveOnesIII.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+int longestOnes(vector<int> &nums, int k)
+{
+    
+}
+int main()
+{
+    vector<int> nums = {1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0};
+    int k = 2;
+    cout << longestOnes(nums, k);
+    return 0;
+}
