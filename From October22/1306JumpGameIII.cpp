24aec5b658ca0001e619d3f6f5048f2273623d13
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-11 23:38:55 +0530

    1306

---
+++ From October22/1306JumpGameIII.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+using namespace std;
+bool canReach(vector<int> &arr, int start)
+{
+    queue<int> qu;
+    qu.push(start);
+    int i;
+    while (!qu.empty())
+    {
+        i = qu.front();
+        cout << "i: " << i << endl;
+        qu.pop();
+        if (arr[i] == 0)
+        {
+            return true;
+        }
+        if (arr[i] + 1 < 0) // if visited
+        {
+            continue;
+        }
+        if (i - arr[i] >= 0)
+        {
+            qu.push(i - arr[i]);
+        }
+        if (i + arr[i] < arr.size())
+        {
+            qu.push(i + arr[i]);
+        }
+        arr[i] = arr[i] * -1;
+    }
+    return false;
+}
+int main()
+{
+    vector<int> arr = {4, 2, 3, 0, 3, 1, 2};
+    int k = 5;
+    cout << canReach(arr, k);
+    return 0;
+}
