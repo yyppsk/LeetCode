bd9a232628389fa76317d66a035c561d61d2a2bf
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-13 00:10:33 +0530

    Find Peak First

--- From October22/81SearchinRotatedSortedArrayII.cpp
+++ From October22/81SearchinRotatedSortedArrayII.cpp
@@ -3,14 +3,29 @@
 #include <algorithm>
 #include <string>
 using namespace std;
+int findpeak(vector<int> &nums)
+{
+    int start = 0, end = nums.size();
+    while (start < end)
+    {
+        int mid = start + (end - start) / 2;
+        // kya mid start wale se bada hai?
+        if (nums[mid] > nums[0])
+            start = mid + 1;
+        else
+            end = mid;
+    }
+    return start;
+}
 bool search(vector<int> &nums, int target)
 {
-    
+    int peak = findpeak(nums);
 }
 int main()
 {
-    vector<int> nums = {2, 5, 6, 0, 0, 1, 2};
+    vector<int> nums = {3, 4, 5, 6, 7, 8, 1, 2};
     int target = 0;
-    cout << search(nums, target);
+    cout << findpeak(nums);
+    // cout << search(nums, target);
     return 0;
 }
