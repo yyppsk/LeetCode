8eef58d0399ba3ca0511005a42fa4f5b031487a8
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-12 21:41:25 +0530

    Function 643

--- From October22/643MaximumAverageSubarrayI.cpp
+++ From October22/643MaximumAverageSubarrayI.cpp
@@ -1,12 +1,16 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
 using namespace std;
-void solve(){
-
+double findMaxAverage(vector<int> &nums, int k)
+{
+    
 }
 int main()
 {
+    vector<int> arr = {1, 12, -5, -6, 50, 3};
+    int k = 4;
+    cout << findMaxAverage(arr, k);
     return 0;
 }
