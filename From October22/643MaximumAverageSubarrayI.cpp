80700db318c286b45009e1f36f8c1a90846a9790
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-12 21:59:49 +0530

    643

--- From October22/643MaximumAverageSubarrayI.cpp
+++ From October22/643MaximumAverageSubarrayI.cpp
@@ -3,9 +3,24 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-double findMaxAverage(vector<int> &nums, int k)
+double findMaxAverageSol1(vector<int> &nums, int k) // O(nk)
 {
-    
+    int n = nums.size();
+    double maxa = INT_MIN;
+    for (int i = 0; i <= n - k; i++)
+    {
+        double sum = 0;
+        // cout << "Sum of : ";
+        for (int j = i; j < i + k; j++)
+        {
+            // printf("%d +", nums[j]);
+            sum += nums[j];
+        }
+        sum /= k;
+        maxa = max(maxa, sum);
+        // printf("is %f\n", sum / 4);
+    }
+    return maxa;
 }
 int main()
 {
