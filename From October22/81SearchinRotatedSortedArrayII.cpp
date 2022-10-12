4c4a6b5f9e394cf1028a0826e99470a4f37343a3
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-12 23:35:49 +0530

    81

---
+++ From October22/81SearchinRotatedSortedArrayII.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+bool search(vector<int> &nums, int target)
+{
+    
+}
+int main()
+{
+    vector<int> nums = {2, 5, 6, 0, 0, 1, 2};
+    int target = 0;
+    cout << search(nums, target);
+    return 0;
+}
