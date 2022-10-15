cc15f2bba1b3a3a153cd9438ee09a5d75cdda03a
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-16 02:15:59 +0530

    pattern = "abba", s = "dog cat cat dog"

---
+++ From October22/290WordPattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+bool wordPattern(string pattern, string s)
+{
+}
+int main()
+{
+    string pattern = "abba", s = "dog cat cat dog";
+    return 0;
+}
