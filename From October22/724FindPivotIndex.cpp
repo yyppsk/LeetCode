b03c1ae18a2ce2e49e4445bbb8c3ebbd73c8ff1e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-15 20:35:46 +0530

    724 Done

--- From October22/724FindPivotIndex.cpp
+++ From October22/724FindPivotIndex.cpp
@@ -5,6 +5,28 @@
 using namespace std;
 int findMiddleIndex(vector<int> &nums)
 {
+    int totalsum = 0;
+    for (auto i : nums)
+    {
+        totalsum += i;
+    }
+    int leftsum = 0;
+    // 1, 7, 3, 6, 5, 6
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int rightsum = totalsum - nums[i] - leftsum;
+        cout << leftsum << " " << rightsum << endl;
+        if (rightsum == leftsum)
+        {
+            return i;
+        }
+        leftsum += nums[i];
+    }
+    return -1;
+}
+/*Two pointer trial approach
+int findMiddleIndex(vector<int> &nums)
+{
     if (nums.size() == 1)
         return 0;
     if (nums.size() == 2)
@@ -39,9 +61,10 @@ int findMiddleIndex(vector<int> &nums)
     cout << "L " << leftsum << " R " << rightsum << endl;
     return end;
 }
+*/
 int main()
 {
-    vector<int> nums = {1, 2, 3};
+    vector<int> nums = {1, 1, 1, 1, 1, 1, 1};
     cout << findMiddleIndex(nums);
     return 0;
 }
