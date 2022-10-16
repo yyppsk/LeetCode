a592091ee55d311a0936d763e206537fc586c6be
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-16 15:03:47 +0530

    Attempt 1

--- From October22/290WordPattern.cpp
+++ From October22/290WordPattern.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 #include <string>
 using namespace std;
 bool wordPattern(string pattern, string s)
 {
+    unordered_map<char, string> mapp;
+    string str = "";
+    vector<string> res;
+    s.push_back(' ');
+    int j = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        str += s[i];
+        if (s[i] == ' ')
+        {
+            mapp[pattern[j]] = str;
+            res.push_back(str);
+            str = "";
+        }
+    }
+    for (auto i : res)
+    {
+        cout << i << " " << endl;
+    }
+    for (auto i : mapp)
+    {
+        cout << i.first << " : " << i.second << endl;
+    }
+    return 0;
 }
 int main()
 {
     string pattern = "abba", s = "dog cat cat dog";
+    cout << wordPattern(pattern, s);
     return 0;
 }
