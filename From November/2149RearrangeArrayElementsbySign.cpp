56a959f0955eb7b8e3e1d24dd76b201cd66bc123
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-11-15 10:36:11 +0530

    Two Pointers 2149

--- From November/2149RearrangeArrayElementsbySign.cpp
+++ From November/2149RearrangeArrayElementsbySign.cpp
@@ -3,24 +3,49 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-vector<int> rearrangeArray(vector<int> &nums)
+vector<int> rearrangeArraybrute(vector<int> &nums)
 {
-    vector<int> pos;
-    vector<int> negatives;
     vector<int> result;
+    int n = 0;
+    vector<int> storage;
+    for (int i : nums)
+    {
+        if (i > 0)
+            storage.push_back(i);
+        n += 1;
+    }
+    n = n / 2;
     for (int i : nums)
     {
         if (i < 0)
-            negatives.push_back(i);
-        else
-            pos.push_back(i);
+            storage.push_back(i);
     }
-    for (int i = 0; i < pos.size(); i++)
+
+    for (int i = 0; i < n; i++)
     {
-        result.push_back(pos[i]);
-        result.push_back(negatives[i]);
+        result.push_back(storage[i]);
+        result.push_back(storage[n + i]);
     }
     return result;
+}
+vector<int> rearrangeArray(vector<int> &nums)
+{   //Two pointers
+    vector<int> res(nums.size(), 0);
+    int pi = 0, ni = 1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] > 0)
+        {
+            res[pi] = nums[i];
+            pi += 2;
+        }
+        else
+        {
+            res[ni] = nums[i];
+            ni += 2;
+        }
+    }
+    return res;
 }
 int main()
 {
