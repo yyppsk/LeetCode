09f8bb1f03de973cb2dc1fdc85dbcae64260015f
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-02-28 14:20:12 +0530

    1876

--- February 2023/1876SubstringsofSizeThreewithDistinctCharacters.cpp
+++ February 2023/1876SubstringsofSizeThreewithDistinctCharacters.cpp
@@ -5,9 +5,17 @@
 using namespace std;
 int countGoodSubstrings(string s)
 {
-    
+    if (s.size() < 3)
+        return 0;
+    int count = 0;
+    for (int i = 0; i < s.size() - 2; i++)
+        if (s[i] != s[i + 1] && s[i] != s[i + 2] && s[i + 1] != s[i + 2])
+            count++;
+    return count;
 }
 int main()
 {
+    string s = "aababcabc";
+    cout << countGoodSubstrings(s) << endl;
     return 0;
 }
