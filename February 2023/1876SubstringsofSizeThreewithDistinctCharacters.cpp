2614ac4c5a2e52ba9393533fe60be70de37ee64a
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-02-28 14:02:03 +0530

    1876

---
+++ February 2023/1876SubstringsofSizeThreewithDistinctCharacters.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+int countGoodSubstrings(string s)
+{
+    
+}
+int main()
+{
+    return 0;
+}
