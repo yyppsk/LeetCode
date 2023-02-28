cabeb9f1d27dabc7b4fc0a1f06a560b2e2e0953a
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-02-28 13:55:31 +0530

    1436 Done

--- February 2023/1436DestinationCity.cpp
+++ February 2023/1436DestinationCity.cpp
@@ -2,14 +2,23 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <unordered_map>
 using namespace std;
 string destCity(vector<vector<string>> &paths)
 {
+    unordered_map<string, string> mp;
     for (auto i : paths)
         for (auto j : i)
         {
-            cout << j << endl;
+            mp.insert({i[0], i[1]});
         }
+    for (int i = 0; i < paths.size(); i++)
+        for (int j = 0; j < paths[i].size(); j++)
+        {
+            if (mp.find(paths[i][j]) == mp.end())
+                return paths[i][j];
+        }
+
     return "Null";
 }
 int main()
