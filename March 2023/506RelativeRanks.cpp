5ecd89dd022ae30d4b636aa2a961db4385977c34
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-03-30 11:35:59 +0530

    506 Done

--- March 2023/506RelativeRanks.cpp
+++ March 2023/506RelativeRanks.cpp
@@ -2,11 +2,53 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <unordered_map>
+
 using namespace std;
 vector<string> findRelativeRanks(vector<int> &score)
 {
+    vector<string> answer;
+    unordered_map<int, int> mp;
+    vector<int> scorenew(score);
+    sort(scorenew.begin(), scorenew.end(), greater<int>());
+    int rank = 1;
+    for (auto i : scorenew)
+    {
+        mp[i] = rank++;
+        cout << i << " ";
+    }
+    cout << endl;
+    for (auto i : mp)
+    {
+        cout << i.first << " : " << i.second << endl;
+    }
+    for (int i = 0; i < score.size(); i++)
+    {
+        if (mp[score[i]] == 1)
+        {
+            answer.push_back("Gold Medal");
+        }
+        else if (mp[score[i]] == 2)
+        {
+            answer.push_back("Silver Medal");
+        }
+        else if (mp[score[i]] == 3)
+        {
+            answer.push_back("Bronze Medal");
+        }
+        else
+            answer.push_back(to_string(mp[score[i]]));
+    }
+    for (auto i : answer)
+    {
+        cout << i << " ";
+    }
+    return {};
 }
 int main()
 {
+    vector<int> score = {10, 3, 8, 9, 4};
+    //["Gold Medal","5","Bronze Medal","Silver Medal","4"]
+    findRelativeRanks(score);
     return 0;
 }
--- March 2023/s.py
+++ March 2023/s.py
@@ -1,13 +1,6 @@
-class stud:
-    def __init__(self, roll_no, grade):
-        self.roll_no = roll_no
-        self.grade = grade
-
-
-def display(self):
-    print("Roll no: ", self.roll_no, ",Grade: ", self.grade)
-
-
-stud1 = stud(34, 'S')
-stud1.age = 7
-print(hasattr(stud1, 'age'))
+l = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flatten = []
+for element in l:
+    for inner_element in element:
+        flatten.append(inner_element)
+print(flatten)
