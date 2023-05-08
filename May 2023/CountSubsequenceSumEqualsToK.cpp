9b1d34a53157dd1f4bdb1123814f01a24efa3e3c
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-05-08 23:05:41 +0530

    CountSubsequenceSumEqualsToK

---
+++ May 2023/CountSubsequenceSumEqualsToK.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+int CountSub(int idx, int arr[], int k, int sum, int n)
+{
+    if (idx == n)
+    {
+        if (sum == k)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    sum += arr[idx];
+    int left = CountSub(idx + 1, arr, k, sum, n);
+    sum -= arr[idx];
+    int right = CountSub(idx + 1, arr, k, sum, n);
+    return left + right;
+}
+int main()
+{
+    int arr[] = {1, 2, 1};
+    int k = 2;
+    cout << CountSub(0, arr, k, 0, 3);
+    return 0;
+}
