2bd16bd18b559f65b9b5391fe654166aa2d63a69
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-05-15 22:15:16 +0530

    98

---
+++ May 2023/98ValidateBinarySearchTree.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Solution
+{
+private:
+    TreeNode *prev = nullptr;
+
+public:
+    bool isValidBST(TreeNode *root)
+    {
+        return inorder(root);
+    }
+
+    bool inorder(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return true;
+        }
+        if (!inorder(root->left))
+        {
+            return false;
+        }
+        if (prev != nullptr && root->val <= prev->val)
+        {
+            return false;
+        }
+        prev = root;
+        return inorder(root->right);
+    }
+};
+int main()
+{
+    return 0;
+}
