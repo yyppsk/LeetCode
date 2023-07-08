5e08b929aa5935733385af016218e69406a82c7e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-07-09 01:13:38 +0530

    2551. Put Marbles in Bags

---
+++ July 2023/2551PutMarblesinBags.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+void solve()
+{
+}
+// 2551. Put Marbles in Bags
+int main()
+{
+    return 0;
+}
