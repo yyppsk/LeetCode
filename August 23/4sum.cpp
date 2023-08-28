f6585a09034ba685700aad18d58a5bbef37bbccc
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-08-29 02:25:15 +0530

    4 Sum

---
+++ August 23/4sum.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    vector<vector<int>> fourSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            for (int j = i + 1; j < n; j++)
+            {
+                if (j != (i + 1) && nums[j] == nums[j - 1])
+                    continue;
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l)
+                {
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if (sum == target)
+                    {
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++, l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                        {
+                            k++;
+                        }
+                        while (k < l && nums[l] == nums[l + 1])
+                        {
+                            l--;
+                        }
+                    }
+                    else if (sum < target)
+                        k++;
+                    else
+                        l--;
+                }
+            }
+        }
+        return ans;
+    }
+    vector<vector<int>> fourSum2(vector<int> &nums, int target)
+    {
+        set<vector<int>> st;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                for (int k = j + 1; k < nums.size(); k++)
+                {
+                    for (int l = k + 1; l < nums.size(); l++)
+                    {
+                        long long sum = nums[i] + nums[j];
+                        sum += nums[k];
+                        sum += nums[l];
+                        if (sum == target)
+                        {
+                            vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                            sort(temp.begin(), temp.end());
+                            st.insert(temp);
+                        }
+                    }
+                }
+            }
+        }
+        vector<vector<int>> result(st.begin(), st.end());
+        return result;
+    }
+};
+int main()
+{
+    return 0;
+}
