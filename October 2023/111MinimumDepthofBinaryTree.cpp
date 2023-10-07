1c6cfc431bad79b24a0406ec7126498902916367
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-10-08 03:31:12 +0530

    111. Minimum Depth of Binary Tree

--- October 2023/111MinimumDepthofBinaryTree.cpp
+++ October 2023/111MinimumDepthofBinaryTree.cpp
@@ -3,17 +3,17 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution
 {
 public:
