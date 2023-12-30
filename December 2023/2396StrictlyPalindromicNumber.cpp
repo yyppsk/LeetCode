a80264bbfcb6552fd6e09664b352e82abeb5fbd5
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-12-30 15:14:16 +0530

    2396. Strictly Palindromic Number

---
+++ December 2023/2396StrictlyPalindromicNumber.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+bool isStrictlyPalindromic(int n)
+{
+    return false;
+}
+int main()
+{
+    return 0;
+}
