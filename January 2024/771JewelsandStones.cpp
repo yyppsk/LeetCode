a3dabf39bfe0fcec47b92667a217b14fc2bf8635
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-01-01 13:38:20 +0530

    771. Jewels and Stones

---
+++ January 2024/771JewelsandStones.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Solution
+{
+public:
+    int numJewelsInStones(string jewels, string stones)
+    {
+        unordered_map<char, int> mp;
+        for (int i = 0; i < stones.size(); i++)
+        {
+            mp[stones[i]]++;
+        }
+        int count = 0;
+        for (int i = 0; i < jewels.size(); i++)
+        {
+            count += mp[jewels[i]];
+        }
+        return count;
+    }
+};
+int main()
+{
+    return 0;
+}
