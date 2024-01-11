9220a4149bc936164dbc4f1b4e51ac780519ab63
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-01-11 20:47:50 +0530

    2769. Find the Maximum Achievable Number

---
+++ January 2024/2769FindtheMaximumAchievableNumber.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <set>
+using namespace std;
+class Solution
+{
+public:
+    int theMaximumAchievableX(int num, int t)
+    {
+        int x = num;
+        x = x + (t * 1);
+        return x + t;
+    }
+};
+int main()
+{
+    return 0;
+}
