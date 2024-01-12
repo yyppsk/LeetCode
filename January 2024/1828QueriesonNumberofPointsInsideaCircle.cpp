d333eb5812079a80af3e4fe86a9e7f6fb6d17efb
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-01-12 17:31:49 +0530

    1828. Queries on Number of Points Inside a Circle

---
+++ January 2024/1828QueriesonNumberofPointsInsideaCircle.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <math.h>
+#include <unordered_map>
+#include <map>
+#include <set>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> countPoints(vector<vector<int>> &points,
+                            vector<vector<int>> &queries)
+    {
+        vector<int> result;
+        for (int i = 0; i < queries.size(); i++)
+        {
+            int q = 0;
+            for (int j = 0; j < points.size(); j++)
+            {
+                int x2 = queries[i][0];
+                int x1 = points[j][0];
+                int y1 = queries[i][1];
+                int y2 = points[j][1];
+                int d1 = pow(x2 - x1, 2);
+                int d2 = pow(y2 - y1, 2);
+                if (d1 + d2 <= pow(queries[i][2], 2))
+                {
+                    q++;
+                }
+            }
+            result.push_back(q);
+        }
+        return result;
+    }
+};
+int main()
+{
+    return 0;
+}
