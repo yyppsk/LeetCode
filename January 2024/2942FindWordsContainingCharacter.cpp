3731f9bdca3bf2f5666c3d707462a5900715501c
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-01-21 02:19:11 +0530

    2942. Find Words Containing Character

--- January 2024/2942FindWordsContainingCharacter.cpp
+++ January 2024/2942FindWordsContainingCharacter.cpp
@@ -6,9 +6,30 @@
 #include <map>
 #include <set>
 using namespace std;
-void solve()
+class Solution
 {
-}
+public:
+    vector<int> findWordsContaining(vector<string> &words, char x)
+    {
+        vector<int> arr;
+        for (int i = 0; i < words.size(); i++)
+        {
+            bool hai = false;
+            for (int j = 0; j < words[i].size(); j++)
+            {
+                if (words[i][j] == x)
+                {
+                    hai = true;
+                }
+            }
+            if (hai)
+            {
+                arr.push_back(i);
+            }
+        }
+        return arr;
+    }
+};
 int main()
 {
     return 0;
