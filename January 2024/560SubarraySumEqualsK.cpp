52859bb344a77e006b999fa118095505fc4fabb8
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-01-29 01:33:26 +0530

    560. Subarray Sum Equals K

--- January 2024/560SubarraySumEqualsK.cpp
+++ January 2024/560SubarraySumEqualsK.cpp
@@ -27,7 +27,7 @@ public:
         }
         return cnt;
     }
-    int subarraySum(vector<int> &nums, int k)
+    int subarraySumNsquare(vector<int> &nums, int k)
     {
         int cnt = 0;
         for (int i = 0; i < nums.size(); i++)
@@ -41,6 +41,24 @@ public:
             }
         }
         return cnt;
+    }
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+
+        long long sum = 0;
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+            if (mp.find(sum - k) != mp.end())
+            {
+                result += mp[sum - k];
+            }
+            mp[sum]++;
+        }
+        return result;
     }
 };
 int main()
