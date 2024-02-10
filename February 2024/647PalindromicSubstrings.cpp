81476a98df00d83f2b4870582552c145fff74363
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-02-10 18:56:01 +0530

    647. Palindromic Substrings

--- February 2024/647PalindromicSubstrings.cpp
+++ February 2024/647PalindromicSubstrings.cpp
@@ -77,6 +77,69 @@ private:
         return t[i][j] = false;
     }
 };
+// Bottom Up
+class Solution
+{
+public:
+    int countSubstrings(string s)
+    {
+        int n = s.size();
+        vector<vector<bool>> t(n, vector<bool>(n, false));
+
+        int count = 0;
+        for (int substrLength = 1; substrLength <= n; substrLength++)
+        {
+            for (int i = 0; i + substrLength - 1 < n; i++)
+            {
+                int j = i + substrLength - 1;
+
+                if (i == j)
+                {
+                    t[i][i] = true;
+                }
+                else if (i + 1 == j)
+                {
+                    t[i][j] = (s[i] == s[j]);
+                }
+                else
+                {
+                    t[i][j] = (s[i] == s[j] && t[i + 1][j - 1]);
+                }
+                if (t[i][j] == true)
+                    count++;
+            }
+        }
+        return count;
+    }
+};
+
+// observation
+
+class Solution
+{
+public:
+    int count = 0;
+    int countSubstrings(string s)
+    {
+        int n = s.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            check(s, i, i, n);
+            check(s, i, i + 1, n);
+        }
+        return count;
+    }
+
+private:
+    void check(string &s, int i, int j, int n)
+    {
+        while (i >= 0 && j < n && s[i++] == s[j++])
+        {
+            count++;
+        }
+    }
+};
 int main()
 {
     return 0;
