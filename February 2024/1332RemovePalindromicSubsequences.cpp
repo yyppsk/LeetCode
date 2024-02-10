a03cd22d1915a84821dd80d7ca48d2aeb3e2c1d6
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-02-10 21:52:35 +0530

    1332. Remove Palindromic Subsequences

--- February 2024/1332RemovePalindromicSubsequences.cpp
+++ February 2024/1332RemovePalindromicSubsequences.cpp
@@ -6,9 +6,24 @@
 #include <map>
 #include <set>
 using namespace std;
-void solve()
+class Solution
 {
-}
+public:
+    int removePalindromeSub(string s)
+    {
+        int start = 0, end = s.size() - 1;
+        while (start <= end)
+        {
+            if (s[start] != s[end])
+            {
+                return 2;
+            }
+            start++;
+            end--;
+        }
+        return 1;
+    }
+};
 int main()
 {
     return 0;
