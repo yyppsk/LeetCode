4c5a022298b0fcaeb6a4789a861ba2405d87b7eb
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-02-14 13:31:21 +0530

    2656. Maximum Sum With Exactly K Elements

---
+++ February 2024/2656MaximumSumWithExactlyKElements.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <set>
+using namespace std;
+class Solution
+{
+public:
+    int maximizeSum(vector<int> &nums, int k)
+    {
+        priority_queue<int> heap;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            heap.push(nums[i]);
+        }
+        int sum = 0;
+        while (k > 0 && !heap.empty())
+        {
+            int m = heap.top();
+            sum += m;
+            heap.pop();
+            heap.push(m + 1);
+            k--;
+        }
+        return sum;
+    }
+};
+int main()
+{
+    return 0;
+}
