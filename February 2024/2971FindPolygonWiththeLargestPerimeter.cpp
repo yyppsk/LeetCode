66a519081bd7c843607acde007c3e745b6ba9841
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-02-15 13:15:00 +0530

    2971. Find Polygon With the Largest Perimeter - Opitimized

--- February 2024/2971FindPolygonWiththeLargestPerimeter.cpp
+++ February 2024/2971FindPolygonWiththeLargestPerimeter.cpp
@@ -7,33 +7,54 @@
 #include <set>
 using namespace std;
 
-long long largestPerimeter(vector<int> &nums)
+class Solution2
 {
-    sort(nums.begin(), nums.end());
-    int low = 0, high = nums.size() - 1;
-    long long maxSum = -1;
-    while (high > 0)
+public:
+    long long largestPerimeter(vector<int> &nums)
     {
-        long long currentWindowSum = 0;
+        sort(nums.begin(), nums.end());
+        int low = 0, high = nums.size() - 1;
+        long long maxSum = -1;
+        while (high > 0)
+        {
+            long long currentWindowSum = 0;
 
-        for (int k = high - 1; k >= 0; k--)
-        {
-            currentWindowSum += nums[k];
+            for (int k = high - 1; k >= 0; k--)
+            {
+                currentWindowSum += nums[k];
+            }
+            if (currentWindowSum > nums[high])
+            {
+                maxSum = max(maxSum, currentWindowSum) + nums[high];
+                break;
+            }
+            high--;
         }
-        if (currentWindowSum > nums[high])
-        {
-            maxSum = max(maxSum, currentWindowSum) + nums[high];
-            break;
-        }
-        high--;
+        return maxSum;
     }
-    return maxSum;
-}
+};
+
+class Solution
+{
+public:
+    long long largestPerimeter(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        long long perimeter = 0, remaining = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] < remaining)
+            {
+                perimeter = nums[i] + remaining;
+            }
+            remaining += nums[i];
+        }
+        return perimeter == 0 ? -1 : perimeter;
+    }
+};
 
 int main()
 {
-    vector<int> nums = {1, 12, 1, 2, 5, 50, 3};
-    cout << "\n"
-         << largestPerimeter(nums);
+
     return 0;
 }
