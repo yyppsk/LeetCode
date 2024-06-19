b4337d325242978774eb169edb88d75a4236839b
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-06-19 23:17:20 +0530

    1482 : Binary Search Solution

--- June 2024/1482MinimumNumberofDaystoMakemBouquets.cpp
+++ June 2024/1482MinimumNumberofDaystoMakemBouquets.cpp
@@ -1,6 +1,52 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
 
+class Solution1
+{
+    int check(vector<int> &bloomDay, int m, int k, int day)
+    {
+        int count = 0;
+        int totalBloom = 0;
+        for (int i = 0; i < bloomDay.size(); i++)
+        {
+            if (bloomDay[i] <= day)
+            {
+                count++;
+            }
+            else
+            {
+                totalBloom += (count / k);
+                count = 0;
+            }
+        }
+
+        totalBloom += (count / k);
+
+        if (totalBloom >= m)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    int minDays(vector<int> &bloomDay, int m, int k)
+    {
+        if (bloomDay.size() < m * k)
+            return -1;
+        int minDay = *min_element(bloomDay.begin(), bloomDay.end());
+        int maxDay = *max_element(bloomDay.begin(), bloomDay.end());
+
+        for (int day = minDay; day <= maxDay; day++)
+        {
+            if (check(bloomDay, m, k, day))
+            {
+                return day;
+            }
+        }
+        return -1;
+    }
+};
+
 int check(vector<int> &bloomDay, int m, int k, int day)
 {
     int count = 0;
@@ -29,19 +75,26 @@ int check(vector<int> &bloomDay, int m, int k, int day)
 }
 int minDays(vector<int> &bloomDay, int m, int k)
 {
-    if (bloomDay.size() < m * k)
+    long long totalBloomNeeded = m * 1LL * k * 1LL;
+    if (bloomDay.size() < totalBloomNeeded)
         return -1;
     int minDay = *min_element(bloomDay.begin(), bloomDay.end());
     int maxDay = *max_element(bloomDay.begin(), bloomDay.end());
 
-    for (int day = minDay; day <= maxDay; day++)
+    while (minDay <= maxDay)
     {
+        long long day = minDay + (maxDay - minDay) / 2;
         if (check(bloomDay, m, k, day))
         {
-            return day;
+            maxDay = day - 1;
+        }
+        else
+        {
+            minDay = day + 1;
         }
     }
-    return -1;
+
+    return minDay;
 }
 int main()
 {
