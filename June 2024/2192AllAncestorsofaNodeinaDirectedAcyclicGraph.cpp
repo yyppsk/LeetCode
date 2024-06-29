9e8eb36ccceaf06ce477f79112d4c6170005dab0
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-06-29 19:42:00 +0530

    2192 - Test Case

--- June 2024/2192AllAncestorsofaNodeinaDirectedAcyclicGraph.cpp
+++ June 2024/2192AllAncestorsofaNodeinaDirectedAcyclicGraph.cpp
@@ -60,17 +60,8 @@ vector<vector<int>> getAncestors(int n, vector<vector<int>> &edges)
 
 int main()
 {
-    int n = 5;
-    vector<vector<int>> edgeList = {{0, 1},
-                                    {0, 2},
-                                    {0, 3},
-                                    {0, 4},
-                                    {1, 2},
-                                    {1, 3},
-                                    {1, 4},
-                                    {2, 3},
-                                    {2, 4},
-                                    {3, 4}};
+    int n = 8;
+    vector<vector<int>> edgeList = {{0, 3}, {0, 4}, {1, 3}, {2, 4}, {2, 7}, {3, 5}, {3, 6}, {3, 7}, {4, 6}};
     getAncestors(n, edgeList);
     return 0;
 }
