af61882c75faac3409b02c75b2c56f50535c1d12
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-07-16 22:08:12 +0530

    236. Lowest Common Ancestor of a Binary Tree

---
+++ July 2024/236LowestCommonAncestorofaBinaryTree.cpp
@@ -0,0 +1,35 @@
+#include <bits/bits-stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+
+    if (root == nullptr || root == p || root == q)
+        return root; // root is null then return null
+
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+    if (left == nullptr)
+        return right;
+
+    else if (right == nullptr)
+        return left;
+    else
+        return root;
+}
+
+int main()
+{
+
+    return 0;
+}
