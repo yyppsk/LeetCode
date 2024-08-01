bf0b5ed442532981d1febaa108d60711e411452c
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-08-01 20:40:21 +0530

    2678. Number of Senior Citizens

---
+++ July 2024/2678NumberofSeniorCitizens.cpp
@@ -0,0 +1,19 @@
+#include <bits/bits-stdc++.h>
+using namespace std;
+
+int countSeniors(vector<string> &details)
+{
+    int count = 0;
+    for (int i = 0; i < details.size(); i++)
+    {
+        int age = ((details[i][11] - '0') * 10) + (details[i][12] - '0');
+        if (age > 60)
+            count++;
+    }
+    return count;
+}
+
+int main()
+{
+    return 0;
+}
