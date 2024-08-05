c7e346ab8bc5d94e9962ce5162615c5b1ef6132f
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-08-05 17:49:29 +0530

    1008. Construct Binary Search Tree from Preorder Traversal

---
+++ August 2024/1008ConstructBinarySearchTreefromPreorderTraversal.cpp
@@ -0,0 +1,44 @@
+#include <bits/bits-stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+int n = 0;
+void build(TreeNode *root, vector<int> &preorder, int idx)
+{
+    if (idx >= n || !root)
+    {
+        return;
+    }
+
+    if (root->val > preorder[idx])
+    {
+        root->left = new TreeNode(preorder[idx]);
+        build(root->left, preorder, idx + 1);
+    }
+    else
+    {
+        root->right = new TreeNode(preorder[idx]);
+        build(root->left, preorder, idx + 1);
+    }
+}
+TreeNode *bstFromPreorder(vector<int> &preorder)
+{
+    TreeNode *root = new TreeNode(preorder[0]);
+    int idx = 1;
+    n = preorder.size();
+    build(root, preorder, idx);
+    return root;
+}
+int main()
+{
+
+    return 0;
+}
