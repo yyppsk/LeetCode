4cafe67224864922fe66572c1cf627a7569e2e1f
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-08-06 22:12:24 +0530

    1409. Queries on a Permutation With Key

--- August 2024/1409QueriesonaPermutationWithKey.cpp
+++ August 2024/1409QueriesonaPermutationWithKey.cpp
@@ -2,8 +2,40 @@
 using namespace std;
 vector<int> processQueries(vector<int> &queries, int m)
 {
+    vector<int> permutation;
+
+    for (int i = 1; i <= m; i++)
+    {
+        permutation.push_back(i);
+    }
+
+    vector<int> result;
+    for (int i = 0; i < queries.size(); i++)
+    {
+        int to_find = queries[i];
+
+        for (int j = 0; j < permutation.size(); j++)
+        {
+            if (permutation[j] == to_find)
+            {
+                result.push_back(j);
+                permutation.erase(permutation.begin() + j);
+                permutation.emplace(permutation.begin(), to_find);
+                break;
+            }
+        }
+    }
+
+    // for (auto &ele : result)
+    // {
+    //     cout << ele << " ";
+    // }
+    return result;
 }
 int main()
 {
+    vector<int> queries = {3, 1, 2, 1};
+    int m = 5;
+    processQueries(queries, m);
     return 0;
 }
