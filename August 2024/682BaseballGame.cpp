6815176962d9e69c28ac2239c858c09d0661c9ce
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-08-24 23:02:48 +0530

    682. Baseball Game -- 2

--- August 2024/682BaseballGame.cpp
+++ August 2024/682BaseballGame.cpp
@@ -1,35 +1,39 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
-int calPoints(vector<string> &operations)
+class Solution
 {
-    vector<int> scores;
+public:
+    int calPoints(vector<string> &operations)
+    {
+        vector<int> scores;
 
-    for (int i = 0; i < operations.size(); i++)
-    {
-        if (operations[i] != "+" || operations[i] != "C" || operations[i] != "D")
+        for (int i = 0; i < operations.size(); i++)
         {
-            scores.push_back(stoi(operations[i]));
+            if (operations[i] != "+" && operations[i] != "C" && operations[i] != "D")
+            {
+                scores.push_back(stoi(operations[i]));
+            }
+            else if (operations[i] == "C" && scores.size() >= 1)
+            {
+                scores.pop_back();
+            }
+            else if (operations[i] == "D" && scores.size() >= 1)
+            {
+                scores.push_back(scores.back() * 2);
+            }
+            else if (operations[i] == "+" && scores.size() >= 2)
+            {
+                scores.push_back(scores.back() + scores[scores.size() - 2]);
+            }
         }
-        else if (operations[i] != "C" && scores.size() >= 1)
+        int res = 0;
+        for (int i = 0; i < scores.size(); i++)
         {
-            scores.pop_back();
+            res += scores[i];
         }
-        else if (operations[i] != "D" && scores.size() >= 1)
-        {
-            scores.push_back(scores.back() * 2);
-        }
-        else if (operations[i] != "+" && scores.size() >= 2)
-        {
-            scores.push_back(scores.back() + scores[scores.size() - 1]);
-        }
+        return res;
     }
-    int res = 0;
-    for (int i = 0; i < scores.size(); i++)
-    {
-        res += scores[i];
-    }
-    return res;
-}
+};
 int main()
 {
     return 0;
