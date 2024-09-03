4e944c42eb21bfabfb86b78fcaf72acfd0759295
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-09-04 00:26:56 +0530

    811. Subdomain Visit Count

--- September 2024/811SubdomainVisitCount.cpp
+++ September 2024/811SubdomainVisitCount.cpp
@@ -3,94 +3,22 @@
 using namespace std;
 vector<string> subdomainVisits(vector<string> &cpdomains)
 {
-    unordered_map<string, int> mp;
-    for (int i = 0; i < cpdomains.size(); i++)
+    unordered_map<string, int> count;
+
+    for (auto cd : cpdomains)
     {
-        stringstream ss(cpdomains[i]);
-        string rep, domain;
-        getline(ss, rep, ' ');
-        ss >> domain;
-
-        stringstream obj(domain);
-        string subDomain;
-        string subdomainCopy = domain;
-        int subDlength = 0;
-        vector<string> parts;
-        while (getline(obj, subDomain, '.'))
-        {
-            parts.push_back(subDomain);
-            subDlength += 1;
-        }
-
-        cout << "SubDlength = " << subDlength << "\n";
-        for (auto &ele : parts)
-        {
-            cout << ele << " ";
-        }
-        cout << "\n";
-
-        if (subDlength == 1)
-        {
-            if (mp.find(parts.back()) == mp.end())
-            {
-                mp[parts.back()] = stoi(rep);
-            }
-            else
-                mp[parts.back()] += 1;
-        }
-        else if (subDlength == 2)
-        {
-            if (mp.find(parts.back()) == mp.end())
-            {
-                mp[parts.back()] = stoi(rep);
-            }
-            else
-                mp[parts.back()] += 1;
-
-            string temp = parts[0] + "." + parts.back();
-
-            if (mp.find(temp) == mp.end())
-            {
-                mp[temp] = stoi(rep);
-            }
-            else
-                mp[temp] += 1;
-        }
-        else
-        {
-            if (mp.find(parts.back()) == mp.end())
-            {
-                mp[parts.back()] = stoi(rep);
-            }
-            else
-                mp[parts.back()] += 1;
-
-            string temp = parts[1] + "." + parts.back();
-
-            if (mp.find(temp) == mp.end())
-            {
-                mp[temp] = stoi(rep);
-            }
-            else
-                mp[temp] += 1;
-
-            if (mp.find(subdomainCopy) == mp.end())
-            {
-                mp[subdomainCopy] = stoi(rep);
-            }
-            else
-                mp[subdomainCopy] += 1;
-        }
+        int i = cd.find(" ");
+        int n = stoi(cd.substr(0, i));
+        string s = cd.substr(i + 1);
+        for (int i = 0; i < s.size(); ++i)
+            if (s[i] == '.')
+                count[s.substr(i + 1)] += n;
+        count[s] += n;
     }
-
-    vector<string> result;
-
-    for (auto &ele : mp)
-    {
-        result.push_back(to_string(ele.second) + " " + ele.first);
-    }
-
-    return result;
+    vector<string> res;
+    for (auto k : count)
+        res.push_back(to_string(k.second) + " " + k.first);
+    return res;
 }
 int main()
 {
