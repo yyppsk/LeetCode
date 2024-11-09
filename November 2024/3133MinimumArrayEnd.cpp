f2a9a7b9c574da005baa137c33083854c0f63c8e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-11-09 21:46:53 +0530

    3133. Minimum Array End

--- November 2024/3133MinimumArrayEnd.cpp
+++ November 2024/3133MinimumArrayEnd.cpp
@@ -1,6 +1,26 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
 
+long long minEndSpace(int n, int x)
+{
+    vector<short> shifts;
+    long long cur = x;
+    long long calc = n - 1;
+
+    for (int i = 0; i < 32; i++) // find bit positions in x which are 0
+        if (!((1 << i) & x))
+            shifts.push_back(i);
+    for (int i = 32; i < 64; i++)
+        shifts.push_back(i);
+
+    // for(auto x: shifts) cout<<x<<' ';
+
+    for (int i = 0; calc > 0; i++, calc >>= 1)
+    {
+        cur += (calc & 1) << shifts[i];
+    }
+    return cur;
+}
 // TLE After 656 / 765 test cases passed.
 long long minEnd_1(int n, int x)
 {
