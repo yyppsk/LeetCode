4dc1a6fd2841e8f2024164132f9bcfe73d5e1e2f
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-12-04 14:57:25 +0530

    116. Populating Next Right Pointers in Each Node

--- December 2024/116PopulatingNextRightPointersinEachNode.cpp
+++ December 2024/116PopulatingNextRightPointersinEachNode.cpp
@@ -16,7 +16,89 @@ public:
         : val(_val), left(_left), right(_right), next(_next) {}
 };
 
-Node *connect(Node *root) {}
+class Solution
+{
+public:
+    Node *connect(Node *root)
+    {
+        Node *prev = root, *curr;
+        while (prev)
+        {
+            curr = prev;
+            while (curr && curr->left)
+            {
+                curr->left->next = curr->right;
+                if (curr->next)
+                    curr->right->next = curr->next->left;
+                curr = curr->next;
+            }
+            prev = prev->left;
+        }
+        return root;
+    }
+};
+
+class Solution2
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (root == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (root->left != nullptr)
+        {
+            root->left->next = root->right;
+        }
+
+        if (root->right != nullptr && root->next != nullptr)
+        {
+            root->right->next = root->next->left;
+        }
+
+        connect(root->left);
+        connect(root->right);
+
+        return root;
+    }
+};
+
+class Solution3
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (root == NULL)
+        {
+            return NULL;
+        }
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int n = q.size();
+            Node *prev = NULL;
+            for (int i = 0; i < n; i++)
+            {
+                auto top = q.front();
+                q.pop();
+                top->next = prev;
+                prev = top;
+                if (top->right)
+                {
+                    q.push(top->right);
+                }
+                if (top->left)
+                {
+                    q.push(top->left);
+                }
+            }
+        }
+        return root;
+    }
+};
 int main()
 {
     return 0;
