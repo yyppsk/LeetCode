be5282f026389a59e678af1aab825e789f2d679e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-12-13 21:36:22 +0530

    2593. Find Score of an Array After Marking All Elements

---
+++ December 2024/2593FindScoreofanArrayAfterMarkingAllElements.cpp
@@ -0,0 +1,45 @@
+#include <bits/bits-stdc++.h>
+using namespace std;
+long long findScore(vector<int> &nums)
+{
+    int n = nums.size();
+    int marked[1000000] = {0};
+    priority_queue<pair<int, int>, vector<pair<int, int>>,
+                   greater<pair<int, int>>>
+        pq;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        pq.push({nums[i], i});
+    }
+    long long score = 0;
+    while (!pq.empty())
+    {
+        if (marked[pq.top().second] == 0)
+        {
+            score += pq.top().first;
+            marked[pq.top().second] = 1;
+
+            if (pq.top().second - 1 >= 0)
+            {
+                marked[pq.top().second - 1] = 1;
+            }
+
+            if (pq.top().second + 1 < n)
+            {
+                marked[pq.top().second + 1] = 1;
+            }
+            pq.pop();
+        }
+        else
+        {
+            pq.pop();
+        }
+    }
+
+    return score;
+}
+
+int main()
+{
+    return 0;
+}
--- December 2024/2779MaximumBeautyofanArrayAfterApplyingOperation.cpp
+++ December 2024/2779MaximumBeautyofanArrayAfterApplyingOperation.cpp
@@ -1,6 +1,8 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
-int maximumBeauty(vector<int> &nums, int k)
+
+// deque
+int maximumBeauty_deque(vector<int> &nums, int k)
 {
     vector<pair<int, int>> intervals;
     for (int i = 0; i < nums.size(); i++)
@@ -22,6 +24,34 @@ int maximumBeauty(vector<int> &nums, int k)
 
         dq.push_back({intervals[i].first, intervals[i].second});
         result = max(result, (int)dq.size());
+    }
+
+    return result;
+}
+
+// queue
+int maximumBeauty_queue(vector<int> &nums, int k)
+{
+    vector<pair<int, int>> intervals;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        intervals.push_back({nums[i] - k, nums[i] + k});
+    }
+
+    sort(intervals.begin(), intervals.end());
+    queue<int> que;
+
+    int result = 0;
+
+    for (int i = 0; i < intervals.size(); i++)
+    {
+        while (!que.empty() && que.front() < intervals[i].first)
+        {
+            que.pop();
+        }
+
+        que.push(intervals[i].second);
+        result = max(result, (int)que.size());
     }
 
     return result;
