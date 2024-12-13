065d68db466850f7c613a5dfe02224808c11fc3d
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-12-13 22:01:35 +0530

    2593. Find Score of an Array After Marking All Elements Array Optimization

--- December 2024/2593FindScoreofanArrayAfterMarkingAllElements.cpp
+++ December 2024/2593FindScoreofanArrayAfterMarkingAllElements.cpp
@@ -1,6 +1,7 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
-long long findScore(vector<int> &nums)
+
+long long findScore2(vector<int> &nums)
 {
     int n = nums.size();
     int marked[1000000] = {0};
@@ -39,6 +40,45 @@ long long findScore(vector<int> &nums)
     return score;
 }
 
+// using uset
+long long findScore(vector<int> &nums)
+{
+    int n = nums.size();
+    unordered_set<int> marked;
+    priority_queue<pair<int, int>, vector<pair<int, int>>,
+                   greater<pair<int, int>>>
+        pq;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        pq.push({nums[i], i});
+    }
+    long long score = 0;
+    while (!pq.empty())
+    {
+        if (marked.find(pq.top().second) == marked.end())
+        {
+            score += pq.top().first;
+            marked.insert(pq.top().second);
+
+            if (pq.top().second - 1 >= 0)
+            {
+                marked.insert(pq.top().second - 1);
+            }
+
+            if (pq.top().second + 1 < n)
+            {
+                marked.insert(pq.top().second + 1);
+            }
+            pq.pop();
+        }
+        else
+        {
+            pq.pop();
+        }
+    }
+
+    return score;
+}
 int main()
 {
     return 0;
