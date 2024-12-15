512a5037a209f426616fc3e2572222c737a3304f
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-12-16 02:01:44 +0530

    Optimal 1792. Maximum Average Pass Ratio

--- December 2024/1792MaximumAveragePassRatio.cpp
+++ December 2024/1792MaximumAveragePassRatio.cpp
@@ -1,5 +1,7 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
+
+// BruteForce ----> Gives MLE
 double maxAverageRatio(vector<vector<int>> &classes, int extraStudents)
 {
     int size = classes.size();
@@ -41,6 +43,45 @@ double maxAverageRatio(vector<vector<int>> &classes, int extraStudents)
     for (int i = 0; i < size; i++)
     {
         answer += ratios[i];
+    }
+
+    return answer / size;
+}
+
+// BETTER
+
+double maxAverageRatio_better(vector<vector<int>> &classes, int extraStudents)
+{
+    int size = classes.size();
+
+    priority_queue<pair<double, int>> pq;
+
+    for (int i = 0; i < size; i++)
+    {
+        double current = (double)classes[i][0] / classes[i][1];
+        double increased =
+            (double)(classes[i][0] + 1) / (classes[i][1] + 1);
+        pq.push({(double)(increased - current), i});
+    }
+
+    while (extraStudents--)
+    {
+
+        int idx = pq.top().second;
+        pq.pop();
+        classes[idx][0] += 1;
+        classes[idx][1] += 1;
+
+        double currentPossibility = (double)classes[idx][0] / classes[idx][1];
+        double newPossiblity = ((double)classes[idx][0] + 1) / (classes[idx][1] + 1);
+
+        pq.push({(double)(newPossiblity - currentPossibility), idx});
+    }
+
+    double answer = 0.0;
+    for (int i = 0; i < size; i++)
+    {
+        answer += (double)classes[i][0] / (double)classes[i][1];
     }
 
     return answer / size;
