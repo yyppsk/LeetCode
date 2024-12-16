6dd4f7c37e2d2b7f995ad7df609da822949958d8
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-12-17 02:39:46 +0530

    Optimal and Stack

--- December 2024/2390RemovingStarsFromaString.cpp
+++ December 2024/2390RemovingStarsFromaString.cpp
@@ -1,7 +1,46 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
 
+// constant space
+
 string removeStars(string s)
+{
+    int index = 0;
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == '*')
+            --index;
+        else
+            s[index++] = s[i];
+    }
+    s.resize(index);
+    return s;
+}
+
+// stack
+string removeStars_stack(string s)
+{
+    string sol;
+    stack<char> st;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '*')
+            st.pop();
+        else
+            st.push(s[i]);
+    }
+
+    while (!st.empty())
+    {
+        sol.push_back(st.top());
+        st.pop();
+    }
+    reverse(sol.begin(), sol.end());
+    return sol;
+}
+
+string removeStars_nonstack(string s)
 {
     string sol;
     for (int i = 0; i < s.length(); i++)
