58a5c516b8b92f3ff900f45491d8fd4bc6363245
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-12-23 21:27:26 +0530

    853. Car Fleet

--- December 2024/853CarFleet.cpp
+++ December 2024/853CarFleet.cpp
@@ -2,8 +2,39 @@
 using namespace std;
 int carFleet(int target, vector<int> &position, vector<int> &speed)
 {
+    int n = position.size();
+    vector<pair<int, double>> carPosTime;
+
+    for (int i = 0; i < n; i++)
+    {
+        carPosTime.emplace_back(position[i],
+                                (double)(target - position[i]) / speed[i]);
+    }
+
+    sort(carPosTime.begin(), carPosTime.end(),
+         [](const pair<int, double> &a, const pair<int, double> &b)
+         {
+             return a.first > b.first;
+         });
+
+    int totalFleet = 0;
+    double previousTime = 0;
+
+    for (auto &car : carPosTime)
+    {
+        if (car.second > previousTime)
+        {
+            totalFleet++;
+            previousTime = car.second;
+        }
+    }
+
+    return totalFleet;
 }
 int main()
 {
+    int target = 12;
+    vector<int> position = {10, 8, 0, 5, 3}, speed = {2, 4, 1, 1, 3};
+    carFleet(target, position, speed);
     return 0;
 }
