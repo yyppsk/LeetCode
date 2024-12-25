e69c1f60a3e5106d9e8b5b5b801765eed7056b8a
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-12-26 03:24:55 +0530

    1079. Letter Tile Possibilities -- No string build

--- December 2024/1079LetterTilePossibilities.cpp
+++ December 2024/1079LetterTilePossibilities.cpp
@@ -1,45 +1,87 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
 
-unordered_map<char, int> frequency;
-unordered_set<char> st;
-int res = 0;
-void all(string curr, int idx, string &tiles)
+// BRUTE
+class Solution
 {
-    if (idx == tiles.size())
+public:
+    unordered_map<char, int> frequency;
+    unordered_set<char> st;
+    int res = 0;
+    void all(string curr, int idx, string &tiles)
     {
-        return;
-    }
-    for (auto &ch : st)
-    {
-        if (frequency[ch] > 0)
+        if (idx == tiles.size())
         {
-            res += 1;
-            curr.push_back(ch);
-            cout << curr << "\n";
+            return;
+        }
+        for (auto &ch : st)
+        {
+            if (frequency[ch] > 0)
+            {
+                res += 1;
+                curr.push_back(ch);
 
-            frequency[ch] -= 1;
-            all(curr, idx + 1, tiles);
+                frequency[ch] -= 1;
+                all(curr, idx + 1, tiles);
 
-            curr.pop_back();
-            frequency[ch] += 1;
+                curr.pop_back();
+                frequency[ch] += 1;
+            }
         }
     }
-}
-int numTilePossibilities(string tiles)
-{
-    for (int i = 0; i < tiles.size(); i++)
+    int numTilePossibilities(string tiles)
     {
-        st.insert(tiles[i]);
-        frequency[tiles[i]] += 1;
-    }
-    all("", 0, tiles);
+        for (int i = 0; i < tiles.size(); i++)
+        {
+            st.insert(tiles[i]);
+            frequency[tiles[i]] += 1;
+        }
+        all("", 0, tiles);
 
-    return res;
-}
+        return res;
+    }
+};
+
+// BETTER
+class Solution_2_Better
+{
+public:
+    unordered_map<char, int> frequency;
+    unordered_set<char> st;
+    int res = 0;
+    void all(int idx, string &tiles)
+    {
+        if (idx == tiles.size())
+        {
+            return;
+        }
+        for (auto &ch : st)
+        {
+            if (frequency[ch] > 0)
+            {
+                res += 1;
+
+                frequency[ch] -= 1;
+                all(idx + 1, tiles);
+
+                frequency[ch] += 1;
+            }
+        }
+    }
+    int numTilePossibilities(string tiles)
+    {
+        for (int i = 0; i < tiles.size(); i++)
+        {
+            st.insert(tiles[i]);
+            frequency[tiles[i]] += 1;
+        }
+        all(0, tiles);
+
+        return res;
+    }
+};
+
 int main()
 {
-    string s = "AAB";
-    numTilePossibilities(s);
     return 0;
 }
