07ef8968595fb15b21fba3b10dbcc1f9f1269207
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-12-26 15:06:17 +0530

    494. Target Sum -- Memoization

--- December 2024/494TargetSum.cpp
+++ December 2024/494TargetSum.cpp
@@ -1,5 +1,7 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
+
+// Brute Recursive
 int res = 0;
 void combination(int curr, int idx, vector<int> &nums, int &target, int &ignore)
 {
@@ -35,6 +37,71 @@ int findTargetSumWays(vector<int> &nums, int target)
         combination(nums[i], 0, nums, target, i);
     }
 }
+
+// Better Recursive
+class Solution
+{
+public:
+    int totalWays = 0;
+
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+        combination(nums, 0, 0, target);
+        return totalWays;
+    }
+
+private:
+    void combination(vector<int> &nums, int currentIndex, int currentSum,
+                     int target)
+    {
+        if (currentIndex == nums.size())
+        {
+            if (currentSum == target)
+            {
+                totalWays++;
+            }
+        }
+        else
+        {
+            combination(nums, currentIndex + 1,
+                        currentSum + nums[currentIndex], target);
+            combination(nums, currentIndex + 1,
+                        currentSum - nums[currentIndex], target);
+        }
+    }
+};
+
+// Memoization
+
+class Solution_Memoization
+{
+public:
+    vector<vector<int>> mem;
+
+    int combination(int curr, int idx, vector<int> &nums, int target)
+    {
+
+        int offset = 1000;
+
+        if (idx == nums.size())
+            return (curr == target) ? 1 : 0;
+
+        if (mem[idx][curr + offset] != -1)
+            return mem[idx][curr + offset];
+
+        int add = combination(curr + nums[idx], idx + 1, nums, target);
+        int subtract = combination(curr - nums[idx], idx + 1, nums, target);
+
+        mem[idx][curr + offset] = add + subtract;
+        return mem[idx][curr + offset];
+    }
+
+    int findTargetSumWays(vector<int> &nums, int target)
+    {
+        mem.assign(nums.size(), vector<int>(2001, -1));
+        return combination(0, 0, nums, target);
+    }
+};
 int main()
 {
     vector<int> nums = {1, 1, 1, 1, 1};
