806f15fae1560c757b501c097ec180585605d4e0
yyppsk <69805496+yyppsk@users.noreply.github.com>
2025-01-05 01:40:09 +0530

    1930. Unique Length-3 Palindromic Subsequences -- Optimal

--- January 2025/1930UniqueLength3PalindromicSubsequences.cpp
+++ January 2025/1930UniqueLength3PalindromicSubsequences.cpp
@@ -37,6 +37,47 @@ int countPalindromicSubsequence(string s)
 
     return subseq;
 }
+
+// precompute
+
+int countPalindromicSubsequence(string s)
+{
+    int n = s.length();
+    vector<pair<int, int>> indices(26, {-1, -1});
+
+    for (int i = 0; i < n; i++)
+    {
+        char ch = s[i];
+        int idx = ch - 'a';
+
+        if (indices[idx].first == -1)
+        {
+            indices[idx].first = i;
+        }
+
+        indices[idx].second = i;
+    }
+
+    int result = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        if (indices[i] == make_pair(-1, -1))
+        {
+            continue;
+        }
+
+        unordered_set<char> st;
+        for (int middle = indices[i].first + 1; middle <= indices[i].second - 1; middle++)
+        {
+
+            st.insert(s[middle]);
+        }
+
+        result += st.size();
+    }
+
+    return result;
+}
 int main()
 {
     string s = "aabca";
