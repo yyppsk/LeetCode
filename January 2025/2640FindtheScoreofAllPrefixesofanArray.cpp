45bc9a61a9cbddfeca06ddf507d965be05f37a59
yyppsk <69805496+yyppsk@users.noreply.github.com>
2025-01-04 15:11:30 +0530

    2640. Find the Score of All Prefixes of an Array

--- January 2025/2640FindtheScoreofAllPrefixesofanArray.cpp
+++ January 2025/2640FindtheScoreofAllPrefixesofanArray.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 vector<long long> findPrefixScore(vector<int> &nums)
 {
+    vector<long long> res;
+    long long currMax = -1;
+    long long sum = 0;
+    long long score = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        long long score = 0;
+
+        currMax = max((long long)nums[i], currMax);
+        sum += currMax + nums[i];
+        score += sum;
+
+        res.emplace_back(score);
+    }
+
+    return res;
 }
 int main()
 {
+    vector<int> arr = {2, 3, 7, 5, 10};
+    findPrefixScore(arr);
     return 0;
 }
