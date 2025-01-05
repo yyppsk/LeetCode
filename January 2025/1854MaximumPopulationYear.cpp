f039f7d64efc330c5fe869672361aa4bbe5bc165
yyppsk <69805496+yyppsk@users.noreply.github.com>
2025-01-06 02:27:19 +0530

    1854. Maximum Population Year -- Optimal

--- January 2025/1854MaximumPopulationYear.cpp
+++ January 2025/1854MaximumPopulationYear.cpp
@@ -1,5 +1,7 @@
 #include <bits/bits-stdc++.h>
 using namespace std;
+
+// TC N*M
 int maximumPopulation(vector<vector<int>> &logs)
 {
     vector<int> years(100, 0);
@@ -15,6 +17,31 @@ int maximumPopulation(vector<vector<int>> &logs)
     int minYear = INT_MAX, maxVal = -1;
     for (int i = 0; i < years.size(); i++)
     {
+        if (years[i] > maxVal)
+        {
+            maxVal = years[i];
+            minYear = i + 1950;
+        }
+    }
+
+    return minYear;
+}
+
+// Optimise
+int maximumPopulation(vector<vector<int>> &logs)
+{
+    vector<int> years(101, 0);
+
+    for (int i = 0; i < logs.size(); i++)
+    {
+        years[logs[i][0] - 1950] += 1; // start
+        years[logs[i][1] - 1950] -= 1; // end
+    }
+
+    int minYear = 1950, maxVal = years[0];
+    for (int i = 1; i < years.size(); i++)
+    {
+        years[i] += years[i - 1];
         if (years[i] > maxVal)
         {
             maxVal = years[i];
