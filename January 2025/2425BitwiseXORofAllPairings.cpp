fd2113a823d9dd112ab893365547b33998d65876
yyppsk <69805496+yyppsk@users.noreply.github.com>
2025-01-16 14:25:53 +0530

    2425. Bitwise XOR of All Pairings -- Optimal with Space

--- January 2025/2425BitwiseXORofAllPairings.cpp
+++ January 2025/2425BitwiseXORofAllPairings.cpp
@@ -29,6 +29,36 @@ int xorAllNums(vector<int> &nums1, vector<int> &nums2)
     return res;
 }
 
+// Optimal
+int xorAllNums(vector<int> &nums1, vector<int> &nums2)
+{
+    unordered_map<int, long> freq;
+
+    int lengthNums1 = nums1.size();
+    int lengthNums2 = nums2.size();
+
+    for (int &ele : nums1)
+    {
+        freq[ele] += lengthNums2;
+    }
+
+    for (int &ele : nums2)
+    {
+        freq[ele] += lengthNums1;
+    }
+
+    int res = 0;
+
+    for (auto &e : freq)
+    {
+        if (e.second % 2 == 1)
+        {
+            res ^= e.first;
+        }
+    }
+    return res;
+}
+
 int main()
 {
     return 0;
