adc95eada892700d793cc2954ac18794d92cc4d9
yyppsk <69805496+yyppsk@users.noreply.github.com>
2025-01-16 14:28:00 +0530

    2425. Bitwise XOR of All Pairings -- Space Optimal

--- January 2025/2425BitwiseXORofAllPairings.cpp
+++ January 2025/2425BitwiseXORofAllPairings.cpp
@@ -59,6 +59,35 @@ int xorAllNums(vector<int> &nums1, vector<int> &nums2)
     return res;
 }
 
+// SPACE OPTIMAL
+
+int xorAllNums(vector<int> &nums1, vector<int> &nums2)
+{
+
+    int xor1 = 0;
+    int xor2 = 0;
+
+    int len1 = nums1.size();
+    int len2 = nums2.size();
+
+    if (len2 % 2 != 0)
+    {
+        for (int num : nums1)
+        {
+            xor1 ^= num;
+        }
+    }
+
+    if (len1 % 2 != 0)
+    {
+        for (int num : nums2)
+        {
+            xor2 ^= num;
+        }
+    }
+
+    return xor1 ^ xor2;
+}
 int main()
 {
     return 0;
